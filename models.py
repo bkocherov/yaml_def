ea279e586df05d0e722916b505ed340251e5f943
Boris Kocherov <bkocherov@gmail.com>
2014-10-29 03:16:12 +0300

    add support yaml

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import yaml
 
-# Create your models here.
+print models.Model.__module__
+yaml_defenitions = yaml.load(open("%s.yaml"%__file__.split('.')[0]).read())
+for klass, value in yaml_defenitions.iteritems():
+    locals()[klass] = type(klass, (models.Model,),{
+                                '__module__':__name__,
+                            })
